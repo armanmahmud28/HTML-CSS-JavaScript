a478396c4380d55b7ae1c83d2133872fa112c325
armanmahmud28 <armanmahmud0028@gmail.com>
2026-03-12 03:51:39 +0600

    learn new topics

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -77,3 +77,55 @@ document.getElementById("obj").innerHTML =
 let find = "age" in person;
 
 document.getElementById("del").innerHTML = find;
+
+//JavaScript Object Constructors
+
+function Person(firstname, lastname, age) {
+  this.firstname = firstname;
+  this.lastname = lastname;
+  this.age = age;
+}
+
+const myself = new Person("Arman", "Mahmud", 21);
+
+document.getElementById("Con").innerHTML =
+  "Name is: " +
+  myself.firstname +
+  " " +
+  myself.lastname +
+  " age is: " +
+  myself.age;
+
+//Date
+
+const d = new Date();
+
+document.getElementById("dd").innerHTML = d;
+document.getElementById("ddd").innerHTML = d.toDateString();
+document.getElementById("dddd").innerHTML = "Year is: " + d.getFullYear();
+document.getElementById("f").innerHTML = "Month: " + d.getMonth();
+
+document.getElementById("ff").innerHTML = "Day: " + d.getDay();
+document.getElementById("fff").innerHTML = "Hours: " + d.getHours();
+
+//JavaScript Temporal:
+
+//Temporal is the new standard for date and time in JavaScript.
+
+const zoned = Temporal.Now.zonedDateTimeISO();
+
+document.getElementById("x").innerHTML = zoned.toString();
+
+//Array
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+document.getElementById("xx").innerHTML = fruits.toString();
+
+const cars = ["Saab", "Volvo", "BMW"];
+let q = "<ul>";
+for (let i = 0; i < cars.length; i++) {
+  q += "<li>" + cars[i] + "</li>";
+}
+q += "</ul>";
+
+document.getElementById("xxx").innerHTML = q;
